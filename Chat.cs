6db3c6af405d42017b77b47927092e396df88112
AdamekPW <szulczynskiadam31@gmail.com>
2024-05-12 16:59:45 +0200

    Obsluga plikow

--- Chat.cs
+++ Chat.cs
@@ -1,24 +1,91 @@
 using System;
 using System.Collections.Generic;
-
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
 public class Chat {
-    private LinkedList<Message> Messages = new LinkedList<Message>();
+
+    public List<string> Users;
+    public LinkedList<Message> Messages = new LinkedList<Message>();
+    public static string ChatsDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Chats");
+    private string ChatFileName = "";
 
     public Chat(){
-        Messages.AddFirst(new Message("Adam ty"));
-        Messages.AddLast(new Message("chuju"));
-        Messages.AddLast(new Message("Dziala"));
+        Users = new List<string>();
+    }
+
+    public Chat(string User1, string User2){
+        Users = new List<string>(){ User1, User2 };
+        ChatFileName = CreateFileName(this.Users);     
+    }
+    public Chat(List<string> Users){
+        this.Users = Users;
+        ChatFileName = CreateFileName(this.Users);
     }
 
     public void AddMessage(Message message){
         Messages.AddLast(message);
     }
 
-    public void Print(){
-        foreach (var item in Messages){
-            Console.WriteLine(item.Data);
+    public string FilePath {
+        get { return Path.Combine(ChatsDirectory, ChatFileName); }
+    }
+
+    
+    private static string CreateFileName(List<string> Users){
+        Users.Sort();
+        string input = "";
+        string ChatFileName;
+        foreach (string user in Users) input += user+"::";
+
+        using (SHA256 sha256Hash = SHA256.Create())
+        {
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+                builder.Append(bytes[i].ToString("x2"));
+            
+            ChatFileName = builder.ToString();
         }
+        return ChatFileName;
+    }
+    
+    
+    public List<Message> GetHistory(int n, LinkedListNode<Message>? Current = null){
+        if (Current == null){
+            Current = Messages.Last;
+        }
+        List<Message> messages = new List<Message>();
+        while (Current != null){
+            messages.Add(Current.Value);
+            Current = Current.Previous;
+        }
+        return messages;
+    }
+    
+    public static void WriteToFile(Chat chat){
+        string jsonData = JsonConvert.SerializeObject(chat, Formatting.Indented);
+        File.WriteAllText(chat.FilePath + ".json", jsonData);
+    }
+
+    public static Chat ReadFromFile(List<string> Users){
+        string FilePath = Path.Combine(ChatsDirectory, CreateFileName(Users));
+        string jsonData = File.ReadAllText(FilePath + ".json");
+        Chat? chat = JsonConvert.DeserializeObject<Chat>(jsonData);
+        if (chat == null){
+            Console.WriteLine("Nie znaleziono chatu, tworzenie nowego");
+            return new Chat();
+        } 
+        return chat;
+       
     }
 
 
+    public void Print(){
+            foreach (var item in Messages){
+                Console.WriteLine(item.Data);
+            }
+        }
 }
--- Program.cs
+++ Program.cs
@@ -1,28 +1,10 @@
-﻿Chat chat = new Chat();
+﻿
+Chat chat = Chat.ReadFromFile(new List<string>(){ "Piotr","Adam", "Basia", "Ola"});
 chat.Print();
 
 
-// Server server = new Server("127.0.0.1", 48025);
-// // //server.StartTunnel();
-// server.Start();
-// Console.ReadLine();
-// CustomClient client = new CustomClient("127.0.0.1", 48025);
-// Message message = new Message("Hello Adam");
-// Thread ClientThread = new Thread(() => client.SendMessage(message));
-// ClientThread.Start();
-
-// // // server.KillTunnel();
-
-// // Console.WriteLine("trying to stop");
-// //server.Stop();
 
 
 
 
 
-
-
-
-
-
-//server.Stop();
